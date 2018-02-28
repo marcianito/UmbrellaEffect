aa37c0eee8c66e4c53cc4c3d942bac9d42e699d3
Marvin Reich <mreich@gfz-potsdam.de>
2018-02-28 07:57:13 +0100

    renaming of varialbes to solve conflict of same names

--- R/reduction_hydScen_SGloc.R
+++ R/reduction_hydScen_SGloc.R
@@ -3,11 +3,11 @@
 #' @description A dynamic correction ratio is created, based on mean soil moisture contents next to the observatory building and the chosen conditons
 #' of a dominant hydrological scenario as well as the position of the SG within its building.
 #'
-#' @param Scenario Character vector, defining the dominant hydrological condition found at or around the observatory building.
+#' @param setScenario Character vector, defining the dominant hydrological condition found at or around the observatory building.
 #' For possible options please read the instructions.
-#' @param SGlocation Character string, describing the position of the SG within its building.
+#' @param setSGlocation Character string, describing the position of the SG within its building.
 #' For possible options please read the instructions.
-#' @param VertLimit Numeric value of the vertical extent to be considered for the reduction. Values are in meters and have to be positive.
+#' @param setVertLimit Numeric value of the vertical extent to be considered for the reduction. Values are in meters and have to be positive.
 #' @param MeanSoilMoisture Data.frame, time series of the mean soil moisture content in the complete space outside of the observatory building.
 #' It needs 2 columns: one for time information (datetime) and one with the soil moisture data (value).
 #' 
@@ -20,11 +20,17 @@
 #' @examples missing
 
 reduction_hydScen_SGloc = function(
-            Scenario,
-            SGlocation,
-            VertLimit,
+            setScenario,
+            setSGlocation,
+            setVertLimit,
             MeanSoilMoisture
 ){
+    ## DEBUGGING
+            # setScenario = Hydro_condition
+            # setSGlocation = SG_position
+            # setVertLimit = VerticalExt_reduction
+            # MeanSoilMoisture = SoilMoisture_mean_ts
+    ##
     # load reduction parameters
     # old file: for complete 5 m vertical model extent
     # load(file="reduction_parameters_HydScen_SGloc.rData")
@@ -32,9 +38,9 @@ reduction_hydScen_SGloc = function(
     load(file="reduction_parameters_HydScen_SGloc_limitedDepth.rData")
     # filter parameterset for chosen settings
     redParam_hydScen_SGloc = dplyr::filter(reduction_parameters,
-                                                  Scenario == Scenario,
-                                                  SGlocation == SGlocation,
-                                                  verticalLimit == VertLimit)
+                                                  Scenario == setScenario,
+                                                  SGlocation == setSGlocation,
+                                                  verticalLimit == setVertLimit)
     factor_ts = MeanSoilMoisture %>%
                 dplyr::mutate(fac1 = redParam_hydScen_SGloc$Intercept + redParam_hydScen_SGloc$Slope * value) %>%
                 dplyr::select(-value)
